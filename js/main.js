8cbff4063aab4a1a7b5865c465ba4a5174586933
Criação 4 <criacao4@Criacao4.local>
2016-07-13 14:00:34 -0300

    Site em construção 13/06

---
+++ js/main.js
@@ -0,0 +1,96 @@
+/* MENU HAMBURGER */
+
+$links = document.querySelectorAll(".menu a"); 
+
+
+for (var i = $links.length - 1; i >= 0; i--) {
+	$links[i].addEventListener("click", function(){
+		$target = this.getAttribute("href");
+		console.log($target);
+		document.querySelector("#menu_opener_id").checked = false;
+	})
+}
+
+/* PLAY VIDEO */
+
+var item = document.querySelectorAll('.rollover');
+
+
+for (var i = 0; i < item.length; i++) {
+    item[i].addEventListener("mouseenter",function(e) {
+    	
+    	var video = this.parentNode.querySelector("video");
+		if (video.paused) {
+    		video.play(); 
+		}
+		else {
+			video.currentTime = 0;
+		}
+	},false);
+
+}
+
+/* FULL SCREEN */
+var button = document.querySelectorAll(".button");
+
+
+for (var i = button.length - 1; i >= 0; i--) {
+	button[i].addEventListener("click", function(e){
+		e.preventDefault();
+
+if (
+	document.fullscreenEnabled || 
+	document.webkitFullscreenEnabled || 
+	document.mozFullScreenEnabled ||
+	document.msFullscreenEnabled
+) {
+	var thisVideo = this.parentNode.querySelector("video");
+		thisVideo.currentTime = 0;
+
+		if (thisVideo.requestFullscreen) {
+		  thisVideo.requestFullscreen();
+		} else if (thisVideo.msRequestFullscreen) {
+		  thisVideo.msRequestFullscreen();
+		} else if (thisVideo.mozRequestFullScreen) {
+		  thisVideo.mozRequestFullScreen();
+		} else if (thisVideo.webkitRequestFullscreen) {
+		  thisVideo.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+		}
+}
+})
+}
+
+/* TIRAR LINK VIDEO */
+
+var ahrefs = document.querySelectorAll(".tiled-work-item a");
+
+for (var i = ahrefs.length - 1; i >= 0; i--) {
+	ahrefs[i].addEventListener("click", function(e){
+		e.preventDefault();
+	})
+}
+
+/* STICKY HEADER */
+
+window.onscroll=function(){changeMenu()}
+
+function changeMenu()
+{
+    var scrollBarPosition = window.pageYOffset | document.body.scrollTop;
+	
+	var el = document.querySelector(".initial");
+    var rect = el.getBoundingClientRect();
+		{
+  			bottom: rect.bottom + document.body.scrollTop
+  		};
+
+  		var header = document.querySelector(".menu_bar");
+
+    // At specifiv position do what you want 
+    if(rect.bottom <= 0) {
+    	header.classList.add("fixo");
+    }
+    else {
+    	header.classList.remove("fixo");
+    }
+}
